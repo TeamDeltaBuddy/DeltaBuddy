45994f08e74768e5b980e376a4a391860898a1d7
TeamDeltaBuddy <mirza.hassanuzzaman@gmail.com>
2025-05-24 12:56:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("pcr").innerText = "1.10";
+document.getElementById("vix").innerText = "12.75";
+document.getElementById("fii").innerText = "+ ₹1,234 Cr";
+document.getElementById("dii").innerText = "- ₹567 Cr";
